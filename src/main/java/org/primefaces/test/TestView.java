7561d67ea96077f80fe5d2f8a0ce4fa5d30c023d
Christian Simon <simon@illucit.com>
2021-10-19 17:25:08 +0200

    Added reproducer: inputnumber trailing space

--- src/main/java/org/primefaces/test/TestView.java
+++ src/main/java/org/primefaces/test/TestView.java
@@ -1,27 +1,26 @@
 package org.primefaces.test;
 
 import java.io.Serializable;
-import java.time.LocalDateTime;
 import java.math.BigDecimal;
-import javax.annotation.PostConstruct;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 
-import lombok.Data;
-
-@Data
 @Named
 @ViewScoped
 public class TestView implements Serializable {
     
-    private String string;
-    private Integer integer;
-    private BigDecimal decimal;
-    private LocalDateTime localDateTime;
-    
-    @PostConstruct  
-    public void init() {
-        string = "Welcome to PrimeFaces!!!";
+    private BigDecimal decimal = new BigDecimal("3.4");
+
+    public BigDecimal getDecimal() {
+        return decimal;
+    }
+
+    public void setDecimal(BigDecimal decimal) {
+        this.decimal = decimal;
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("New value: " + getDecimal()));
     }
 
 }
